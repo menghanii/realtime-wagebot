7520382bd6fbab528412a9e304329dc1d32780e2
menghanii <suneverset@naver.com>
2021-08-12 19:54:08 +0900

    title changed

--- static/myjs.js
+++ static/myjs.js
@@ -19,15 +19,13 @@ function getWage(){
                 var parser = new DOMParser();
                 var htmlDoc = parser.parseFromString(val,"text/html");
                 var wage = htmlDoc.getElementById("wage");
-                var title_wage = htmlDoc.getElementById("real_wage").value;
+                var title_wage = htmlDoc.title;
                 // var title_wage = htmlDoc.getElementById("title_wage");
                 var divToBeChanged = document.getElementById("wage");
                 // var titleToBeChanged = document.getElementById("title_wage");
                 divToBeChanged.innerHTML = wage.innerHTML;
                 // titleToBeChanged.innerHTML = title_wage.innerHTML;
                 document.title = title_wage;
-                console.log(title_wage);
-
             } else {
             // handle errors
             }
